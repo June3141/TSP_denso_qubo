a8a3a25241b1a46fc40b8e0d1858e44c3d82dd23
june3141 <ryoushi0620@gmail.com>
2020-03-08 20:10:32 +0900

    計算結果を画像化する関数に渡すようにした

--- src/main.py
+++ src/main.py
@@ -1,8 +1,39 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 import pyqubo
 from demo.fakeData import makeDemoData
+from plot.plotMap import plotMap
 from pyquboSolver.tsp.costHamiltonian import TSPHamiltonian, makeHamiltonian
+
+
+def dict2AnsList(solutionsDict, citiesSize):
+    ansDicts = [solutionsDict["x"][i] for i in range(citiesSize - 1)]
+    orderList = [key for i in range(citiesSize - 1) for key, value in ansDicts[i].items() if value == 1]
+
+    if len(orderList) != (citiesSize - 1):
+        print(
+            """
+            Solutions doesn't fulfill requirements.
+            There are cities you don't visit.
+        """
+        )
+        sys.exit()
+
+    for i in range(citiesSize - 1):
+        if i not in orderList:
+            print(
+                """
+                Solutions doesn't fulfill requirements.
+                There are cities you visit more than once.
+            """
+            )
+            sys.exit()
+
+    return orderList
+
 
 if __name__ == "__main__":
     cities = 5
@@ -10,7 +41,7 @@ if __name__ == "__main__":
     citiesLocation, paths = makeDemoData(citiesSize=cities)
 
     hamiltonian = TSPHamiltonian(citiesSize=cities, pathsWeight=paths)
-    feedDict = {"lamTime": 10.0, "lamVisit": 10.0}
+    feedDict = {"lamTime": 250.0, "lamVisit": 250.0}
 
     qubo, offset = hamiltonian.compile().to_qubo(feed_dict=feedDict)
     rawSolution = pyqubo.solve_qubo(qubo)
@@ -18,4 +49,10 @@ if __name__ == "__main__":
         rawSolution, vartype="BINARY", feed_dict=feedDict
     )
 
-    print(decodeSolution)
+    orderList = dict2AnsList(solutionsDict=decodeSolution, citiesSize=cities)
+    orderList = list(map(lambda x: x + 1, orderList))
+    orderList.append(0)
+
+    plotMap(citiesLocation=citiesLocation, orderList=orderList)
+
+    sys.exit()
